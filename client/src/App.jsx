a5c83722261dc9733450b12fbc316144e52b2642
Abdullah <as794614@gmail.com>
2024-10-11 19:14:28 +0530

    name added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 function App() {
-  return <h1 className="text-3xl font-bold underline">Mushahid</h1>;
+  return <h1 className="text-3xl font-bold underline">Mushahid,abdullah</h1>;
 }
 
 export default App;
